49e714ab264204ddb2b7e9a548b1a18ac3c4c509
julinha13 <ju.amorim13@gmail.com>
2020-04-30 21:22:40 -0300

    start of layout definition - jsx and css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator/>
+  </div>,  
   document.getElementById('root')
 );
 
@@ -15,3 +16,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//<React.StrictMode>
