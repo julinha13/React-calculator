8c18cd20bb15c5bd24c41f80a9805ed2817b1fe7
julinha13 <ju.amorim13@gmail.com>
2020-05-02 19:28:40 -0300

    getting button label on click && creating basic funcs os calculator && adjusting caculator css

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css'
 
 export default props =>
-{/* `` let me use javascript code*/}
+/* `` let me use javascript code*/
 
 <button 
 onClick={e => props.click && props.click(props.label)}
