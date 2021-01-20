a7b45464a245bb6a8508051b5a20917b2eeedb1c
julinha13 <ju.amorim13@gmail.com>
2021-01-20 12:01:30 -0300

    removing deafault unnecessary images and service worker

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
 
 import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-ReactDOM.render(
+render(
   <div>
     <h1>Calculadora</h1>
     <Calculator/>
-  </div>,  
+  </div>, 
+
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-//<React.StrictMode>
